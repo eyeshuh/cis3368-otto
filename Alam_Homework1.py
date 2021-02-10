b5c9fbb8751dec62a3baafe947da56a034334a80
ayshaalam <ayshaalam@users.noreply.github.com>
2021-02-09 21:46:21 -0600

    Defining user choice

--- Alam_Homework1.py
+++ Alam_Homework1.py
@@ -2,6 +2,11 @@
 # CIS 3368 - Otto
 # Homework 1
 
+# Implement a menu of options for a user to modify the contact details
+# Each option is represented by a single character
+# The program initially outputs the menu, and outputs the menu after a user chooses an option
+# The program ends when the user chooses the option to Quit. This should look like this
+
 def menu_output():
     print("MENU")
 
@@ -16,4 +21,12 @@ def menu_output():
     print("Choose an option: ")
 
 menu_output()
+
+def user_choice():
+    user_choice = input()
+
+    if user_choice == "a":
+        print("Add contact")
+
+user_choice()
 
