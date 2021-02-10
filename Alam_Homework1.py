f4432303326505560edd3414f4b75cf4d1e70242
ayshaalam <ayshaalam@users.noreply.github.com>
2021-02-09 21:52:20 -0600

    Completing user choice queries

--- Alam_Homework1.py
+++ Alam_Homework1.py
@@ -5,7 +5,6 @@
 # Implement a menu of options for a user to modify the contact details
 # Each option is represented by a single character
 # The program initially outputs the menu, and outputs the menu after a user chooses an option
-# The program ends when the user chooses the option to Quit. This should look like this
 
 def menu_output():
     print("MENU")
@@ -27,6 +26,16 @@ def user_choice():
 
     if user_choice == "a":
         print("Add contact")
+    elif user_choice == "d":
+        print("delete contact")
+    elif user_choice == "u":
+        print("update contact")
+    elif user_choice == "b":
+        print("output all contacts in ALPHA")
+    elif user_choice == "c":
+        print("Output all contacts by creatation date")
+    elif user_choice == "o":
+        print("Output all contacts")
 
 user_choice()
 
