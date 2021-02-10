9034da8585906bbe1686642e398e8e82020f238f
ayshaalam <ayshaalam@users.noreply.github.com>
2021-02-09 21:31:59 -0600

    user test file commit

---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, id, firstname, lastname, dateofbirth):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.dateofbirth = dateofbirth
