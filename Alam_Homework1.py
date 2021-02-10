360e4ec961ff36a71e806b5d0cd10143f91842c6
ayshaalam <ayshaalam@users.noreply.github.com>
2021-02-09 21:33:18 -0600

    Defining menu and initilizing hw 1

---
+++ Alam_Homework1.py
@@ -0,0 +1,19 @@
+# Aysha Alam
+# CIS 3368 - Otto
+# Homework 1
+
+def menu_output():
+    print("MENU")
+
+    print("a - Add contact")
+    print("d - Delete contact")
+    print("u - Update contact details")
+    print("b - Output all contacts in alphabetical order")
+    print("c - Output all contacts by creatation date")
+    print("o - Output all contacts")
+    print("q - Quit\n")
+
+    print("Choose an option: ")
+
+menu_output()
+
