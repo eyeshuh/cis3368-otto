f21809df220f19f900ebe940542d2ac9cbd8a88e
ayshaalam <ayshaalam@users.noreply.github.com>
2021-02-10 21:21:39 -0600

    Creating a function to establish and verify DB connection

--- Alam_Homework1.py
+++ Alam_Homework1.py
@@ -6,6 +6,22 @@
 # Each option is represented by a single character
 # The program initially outputs the menu, and outputs the menu after a user chooses an option
 
+def db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+        
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
 def menu_output():
     print("MENU")
 
