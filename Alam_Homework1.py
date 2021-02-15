aadd8a72f1e70ef66c3641ad592fc7f99b70e81b
ayshaalam <ayshaalam@users.noreply.github.com>
2021-02-14 21:25:21 -0600

    Initiating output menu, add contact menu, and implemented quit to break program

--- Alam_Homework1.py
+++ Alam_Homework1.py
@@ -10,63 +10,84 @@
 #from datetime import date
 
 import mysql.connector
-from mysql.connector import Error
+
+import datetime
+from datetime import date
+
 
 class Contacts:
-    def __init__(self, id, contactDetails, creationDate):
-        self.id = id
+    def __init__(self, contact_id, contactDetails, creationDate):
+        self.id = contact_id
         self.contactDetails = contactDetails
-        self.creationDate = creationDate 
+        self.creationDate = creationDate
 
-def db_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-        
-    except Error as e:
-        print(f"The error '{e}' occurred")
 
-    return connection
+my_db = mysql.connector.connect(
+    host="db connection address",
+    user="username",
+    password="password",
+    database="dbname"
+)
+
+db_cursor = my_db.cursor()
+
 
 def menu_output():
     print("MENU")
-    print("a - Add contact")
+    print("a - Add contact")  # Done
     print("d - Delete contact")
     print("u - Update contact details")
     print("b - Output all contacts in alphabetical order")
     print("c - Output all contacts by creatation date")
-    print("o - Output all contacts")
-    print("q - Quit\n")
+    print("o - Output all contacts")  # Done
+    print("q - Quit\n")  # Done
 
     print("Choose an option: ")
 
-menu_output()
 
-def user_choice():
-    user_choice = input()
+def main():
+    menu_output()
+    user_input = str(input())
 
-    if user_choice == "a":
-        print("Add contact")
-    elif user_choice == "d":
-        print("delete contact")
-    elif user_choice == "u":
-        print("update contact")
-    elif user_choice == "b":
-        print("output all contacts in ALPHA")
-    elif user_choice == "c":
-        print("Output all contacts by creatation date")
-    elif user_choice == "o":
-        print("Output all contacts")
+    # A - ADD CONTACTS
 
-user_choice()
+    if user_input == 'a':
+        add_contact = str(input("Please enter full name: "))
+        add_date = str(input("Please enter date as yyyy/mm/dd"))
 
-def add_contact():
-    query = "INSERT INTO contacts (contactDetails, creationDate) VALUES ('Thomas Edison', '05-08-2021'" + str_dob + "')"
+        sql_query_add_contact = "INSERT INTO contacts (contactDetails, creationDate) VALUES (%s, %s)"
+        sql_query_add_contact_value = (add_contact, add_contact)
+
+        db_cursor.execute(sql_query_add_contact, sql_query_add_contact_value)
+        my_db.commit()
+        print(db_cursor.rowcount, "contact inesrted")
+
+    # C - OUTPUT BY CREATION DATE
+
+    # O - OUTPUT ALL CONTACTS
+
+    elif user_input == 'o':
+        output_all_sql = "SELECT * FROM contacts"
+        db_cursor.execute(output_all_sql)
+        #my_db.commit()
+        print("All contacts:\n")
+
+        db_cursor.fetchall()
+        print("Total number of contacts is: ", db_cursor.rowcount)
+
+        for row in records:
+            print("ID: ", row[0])
+            print("contactDetails: ", row[1])
+            print("creationDate: ", row[2], "\n")
+
+    # Q - QUIT PROGRAM
+
+    elif user_input == 'q':
+        quit
 
 
+main()
+
+
+# references
+# https://www.w3schools.com/python/python_mysql_insert.asp
