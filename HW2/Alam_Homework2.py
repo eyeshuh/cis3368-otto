8169352248e793ddd748d901ced8cd534b7a3455
ayshaalam <ayshaalam@users.noreply.github.com>
2021-03-02 21:49:41 -0600

    setting up API

--- HW2/Alam_Homework2.py
+++ HW2/Alam_Homework2.py
@@ -5,3 +5,11 @@
 import requests
 import json
 import mysql.connector
+
+
+data_request = requests.get("https://api.propublica.org/congress/v1/bills/subjects/taxation.json", headers = {'X-API-Key': '5ItJh3Z1KOmu2yCRe0klswSzzdDq100ZCjzcedXq'})
+print (data_request.json()['results'][0])
+
+
+
+
