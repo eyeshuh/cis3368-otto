adf6e96f1e5e71071b975f8e804ed6d99f9fc068
ayshaalam <ayshaalam@users.noreply.github.com>
2021-03-06 20:58:27 -0600

    defining the menu with weather api

--- HW2/Alam_Homework2.py
+++ HW2/Alam_Homework2.py
@@ -5,21 +5,13 @@
 import requests
 import json
 import mysql.connector
+import math
 
 # 5fb8dd6b3ff2fb635e104eb9e0d4ac2f
 
-data_request = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Houston,Texas&appid=5fb8dd6b3ff2fb635e104eb9e0d4ac2f").json()
-print(data_request)
-print("\n\n")
+data_request = requests.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=Houston,Texas&units=imperial&appid=5fb8dd6b3ff2fb635e104eb9e0d4ac2f").json()
 
-print(data_request['weather'])
-print("\n\n")
-
-print(data_request['weather'][0]['main'])
-print("\n\n")
-
-print(data_request['weather'][0]['description'])
-print("\n\n")
 
 def menu():
     print("c - Display current weather conditions")
@@ -31,16 +23,31 @@ def menu():
     print("a - Add record to Database")
     print("v - View database")
     print("q - Quit\n")
+
     print("Choose an option: ")
 
-while True:
-    menu()
-    user_input = str(input())
-    
-    if user_input == 'q':
-        break
 
+menu()
+user_input = str(input())
 
+if user_input == 'f':
+    feels_like = data_request['main']['feels_like']
+    feels_like = math.floor(feels_like)
+    print(feels_like, "degrees")
+
+elif user_input == 'h':
+    print("The current humidity outside is: ",
+          data_request['main']['humidity'], "%")
+
+elif user_input == 'm':
+    min_temp = data_request['main']['temp_min']
+    min_temp = math.floor(min_temp)
+
+    max_temp = data_request['main']['temp_max']
+    max_temp = math.floor(max_temp)
+
+    print("Today's low is: ", min_temp, "degrees")
+    print("Today's high is: ", max_temp, "degrees")
 
 
 # relevant parts of API #
@@ -49,6 +56,3 @@ while True:
 # print(data_request['weather'][0]['main'])
 # weather main, temp (t- main temp), description (weather.desc), feels like (main), min and max for day (main min and max)
 # humidity (main), sunrise and sunset (sys)
-
-
-
